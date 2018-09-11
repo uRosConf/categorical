49fcf3502238460682bcf95e1d2174caeeefce87
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2018-09-11 15:40:57 +0200

    Presentation

--- R/print.R
+++ R/print.R
@@ -1,14 +1,18 @@
 
 #' @export
-print.classification <- function(x, ...) {
+print.classification <- function(x, short = FALSE, ...) {
   cat("Classification with ", length(x), " levels\n", sep = "")
   for (i in seq_along(x)) {
-    cat("\n=== Level ", i, ": ", nrow(x[[i]]), " categories.\n", sep = "")
-    if (nrow(x[[i]]) > 20) {
-      print(utils::head(x[[i]], 10))
-      cat("...\n")
-    } else {
-      print(x[[i]])
+    if (short) {
+      cat("Level ", i, ": ", nrow(x[[i]]), " categories.\n", sep = "")
+    } else{
+      cat("\n=== Level ", i, ": ", nrow(x[[i]]), " categories.\n", sep = "")
+      if (nrow(x[[i]]) > 20) {
+        print(utils::head(x[[i]], 10))
+        cat("...\n")
+      } else {
+        print(x[[i]])
+      }
     }
   }
 }
